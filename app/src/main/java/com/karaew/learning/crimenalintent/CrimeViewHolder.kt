c3530be2324dba83c732f2833de4467cd1558819
Theyoo <andreyka.karaev.1999@mail.ru>
2023-01-12 21:48:13 +0300

    add Model,Fragment,View components: Button,TextView,EditText

--- app/src/main/java/com/karaew/learning/crimenalintent/CrimeViewHolder.kt
+++ app/src/main/java/com/karaew/learning/crimenalintent/CrimeViewHolder.kt
@@ -1,6 +1,6 @@
 package com.karaew.learning.crimenalintent
 
-import android.util.Log
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,31 +8,45 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 
-class CrimeViewHolder(view: View) : RecyclerView.ViewHolder(view),View.OnClickListener {
+class CrimeViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
     private lateinit var crime: Crime
     private val titleTextView: TextView = itemView.findViewById(R.id.crime_title)
     private val dateTextView: TextView = itemView.findViewById(R.id.crime_date)
-    init {
-        itemView.setOnClickListener { this }
-    }
+
     fun bind(crime: Crime) {
         this.crime = crime
         titleTextView.text = this.crime.title
         dateTextView.text = this.crime.date.toString()
     }
 
-    override fun onClick(v: View?) {
-      // Toast.makeText(context,"${crime.title} pressed",Toast.LENGTH_SHORT)
+    init {
+        itemView.setOnClickListener {
+            Toast.makeText(it.context, "Pressed ${crime.title}", Toast.LENGTH_SHORT).show()
+        }
     }
+
+
 }
 
 class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CrimeViewHolder {
+        return when (viewType) {
+            1 -> {
+                val view =
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.list_item_crime, parent, false)
+                CrimeViewHolder(view)
+            }
+            else -> {
+                val view =
+                    LayoutInflater.from(parent.context)
+                        .inflate(R.layout.list_item_critical, parent, false)
+                CrimeViewHolder(view)
+            }
+        }
 
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.list_item_crime, parent, false)
-        return CrimeViewHolder(view)
+
     }
 
     override fun onBindViewHolder(holder: CrimeViewHolder, position: Int) {
@@ -41,5 +55,11 @@ class CrimeAdapter(var crimes: List<Crime>) : RecyclerView.Adapter<CrimeViewHold
     }
 
     override fun getItemCount() = crimes.size
+    override fun getItemViewType(position: Int): Int {
+        return when (crimes[position].requarePolice) {
+            true -> 1
+            else -> 2
+        }
+    }
 
 }
